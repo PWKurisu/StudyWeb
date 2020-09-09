a5b56561ae6253d34c6dceeca583b0d442bbd2f3
Kurisu <pjs0523gg@gmail.com>
2020-09-09 20:59:01 +0900

    study nodejs

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,40 +2,60 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+    return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(filelist) {
+    var list = '<ul>';
+    for(var i = 0; i < filelist.length; i++) {
+        var filename = filelist[i].substring(0, filelist[i].length-4);
+        list = list + `<li><a href="/?id=${filename}">${filename}</a></li>`;
+    }
+    list = list + '</ul>';
+    return list;
+}
+
 var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/') {
-        title = 'Welcome';
+    var pathname = url.parse(_url, true).pathname;
+    var id = queryData.id;
+
+    var pathFiles = 'C:/Users/Kurisu/HTMLWorkspace/StudyWeb/nodejs/files';
+
+    if(pathname == '/') {
+        if(id === undefined) {
+            fs.readdir(pathFiles, function(error, filelist) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.Js';
+                response.writeHead(200);
+                response.end(templateHTML(title, templateList(filelist), `<h2>${title}</h2><p>${description}</p>`));
+            });
+        } else {
+            fs.readdir(pathFiles, function(error, filelist) {
+                fs.readFile(pathFiles+`/${id}.txt`, 'utf8', (error, description) => {
+                    response.writeHead(200);
+                    response.end(templateHTML(id, templateList(filelist), `<h2>${id}</h2><p>${description}</p>`));
+                });
+            });
+        }
+    } else {
+      response.writeHead(404);
+      response.end('Not Found')
     }
-    if(_url == '/favicon.ico') {
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile('C:/Users/Kurisu/HTMLWorkspace/mymam/nodejs/files/sample.txt', 'utf8', (err, data) => {
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${data}</p>
-        </body>
-        </html>
-        `;
-        response.end(template);
-    });
 });
 app.listen(80);
